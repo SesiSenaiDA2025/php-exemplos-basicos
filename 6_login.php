fb7cb7de61012e717b832f64777360fed40d4cc8
SesiSenaiDA2025 <sesisenaida2025@gmail.com>
2024-10-29 08:56:14 -0300

    EX_6
    EX_6

--- 6_login.php
+++ 6_login.php
@@ -16,8 +16,45 @@
     </form>
 
     <?php
+    // Verifica se o formulário foi enviado
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    // Digitar PHP (1º Aqui)
+        // Recebe os valores e armazena nas variáveis
+        $nome = $_POST['nome'];
+        $senha = $_POST['senha'];
+    }
+
+    // Abre o arquivo (usuarios.txt) para leitura
+    $arquivo = fopen('usuarios.txt', 'r');
+
+    // Assumímos que o úsuario ainda não deve acessar
+    // porque ainda não comparamos (validamos a entrada)
+    // ou seja (segurança)
+    $login_sucesso = false;
+
+    // Lê cada linha do arquivo
+    while (($linha =fgets($arquivo)) !== false) {
+
+        // Divide a linha pelo delimitador ";"
+        list($usuario_arquivo, $senha_arquivo) = explode(';', trim($linha));
+
+        // Verifica se o nome e senha correspondem aos valores no arquivo
+        if ($nome == $usuario_arquivo && $senha == $senha_arquivo) {
+            $login_sucesso = true;
+            break;
+        }
+    }
+    // Fecha o arquivo
+    fclose($arquivo);
+
+    // Mensagem (Feedback) ao usuario
+    if ($login_sucesso) {
+
+        echo "<p>Login realizado com sucesso! Bem-vindo, $nome!</p>";
+
+    } else {
+        echo "<p style='color: red;'>Usuario ou senha incorretos!!</p>";
+    }
     
     ?>
 </body>
